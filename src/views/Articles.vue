32c60a662733dd50f4d16c1c109bd039041ddc29
cph <221830224@smail.nju.edu.cn>
2025-01-10 00:59:03 +0800

    update Articles

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="p-4">
+    <!-- 添加标题 -->
+    <h2 class="text-gray-400 text-center text-lg font-semibold mb-4">
+      ——文章列表——
+    </h2>
+    <!-- MarkDownFinder 组件 -->
     <MarkDownFinder />
   </div>
 </template>
